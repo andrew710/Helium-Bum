1c33ca07215dc3c991e897320a12be0f60df10ae
Andrew Ha <Andrew710@gmail.com>
2017-10-24 11:47:11 -0700

    Initial commit

---
+++ Assets/Scripts/Add_Helium.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Add_Helium : MonoBehaviour {
+
+    public Text helium_text;
+	// Use this for initialization
+	void Start () {
+        //helium_text.text = "helium: " + Balloon_Script.helium;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/Balloon_Script.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Balloon_Script : MonoBehaviour {
+    public double helium;
+    public Text helium_text;
+
+
+    public Rigidbody2D rbody;
+
+	// Use this for initialization
+	void Start () {
+        helium_text.text = "Helium: " + helium;
+        rbody.GetComponent<Rigidbody2D>();
+        rbody.gravityScale *= (float)(helium * 0.025);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // if (rbody.gravityScale <= 1)
+             
+            rbody.gravityScale += (float)(0.25 * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            helium += (10 * Time.deltaTime);
+            helium_text.text = "helium: " + System.Math.Truncate(helium * 100) / 100;
+        }
+    }
+}

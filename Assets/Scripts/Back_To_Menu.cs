61f0d3cf81f2924362806bb5dada905c09797641
Andrew Ha <Andrew710@gmail.com>
2018-01-07 15:41:58 -0800

    Added Start Menu

---
+++ Assets/Scripts/Back_To_Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Back_To_Menu : MonoBehaviour {
+
+public void mainMenu()
+    {
+        SceneManager.LoadScene("start menu");
+    }
+}
